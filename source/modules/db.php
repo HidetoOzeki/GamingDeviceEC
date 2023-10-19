1d010035ad0407d2be966c9e0d97fc246a7bc287
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2023-10-19 23:01:36 +0900

    フォルダ構成、基本的なモジュールの追加、検証のためのダミーファイル
    source/
    →ソースフォルダ、各画面のphpファイルなど
    source/modules/
    →モジュールフォルダ。ヘッダ、フッター、DB接続定数など
    source/script/
    →スクリプトフォルダ。JS,VueJSのファイル
    source/style/
    →CSSを保存するフォルダ

---
+++ source/modules/db.php
@@ -0,0 +1,8 @@
+<!-- @英斗 誰のデータベース使うん？ -->
+<?php
+ const SERVER = '';
+ const DBNAME = '';
+ const USER = '';
+ const PASS = '';
+ $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
+?>
