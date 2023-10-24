5a1e15d027ac4b93bf38d4c584114a3389e5ebe5
fsho1222 <148302976+fsho1222@users.noreply.github.com>
2023-10-24 12:34:50 +0900

    ログイン画面 追加

---
+++ source/login.php
@@ -0,0 +1,15 @@
+<?php require 'modules/header.php'; ?>
+
+    <h1>ログイン</h1>
+
+    <p>メールアドレス</p>
+    <input placeholder="メールアドレス" type="text">
+
+    <p>パスワード</p>
+    <input placeholder="パスワード" type="password">
+
+    <br>
+    <button class="centered_button">新規登録</button>
+
+<?php require 'modules/navigation.php'; ?>
+<?php require 'modules/footer.php'; ?>
