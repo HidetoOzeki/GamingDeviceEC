1a1edd7e4a4abafc50c954dabb33af3fee244d22
Ryusei081 <2201125@s.asojuku.ac.jp>
2023-10-24 12:46:05 +0900

    プロフィール

---
+++ source/profile.php
@@ -0,0 +1,10 @@
+<?php require 'modules/header.php'; ?>
+ <p><h1>プロフィール編集</h1></p>
+ <i class="fas fa-user-circle"></i>
+ <p><button class="" name="button">保存</button></p>
+ <p>ユーザーネーム</p>
+    <input placeholder="ユーザーネーム" type="text">
+ <p>パスワード</p>
+    <input placeholder="パスワード" type="password">
+<?php require 'modules/navigation.php'; ?>
+<?php require 'modules/footer.php'; ?>
