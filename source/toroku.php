6ec717dd7dda88068b39c6220365f323985c7f9c
fsho1222 <148302976+fsho1222@users.noreply.github.com>
2023-10-26 11:48:40 +0900

    削除・登録完了画面

---
+++ source/toroku.php
@@ -0,0 +1,9 @@
+<?php require 'modules/header.php'; ?>
+
+    <h3>登録が完了しました</h3>
+
+    <br>
+    <button class="centered_button">一覧に戻る</button>
+
+<?php require 'modules/navigation.php'; ?>
+<?php require 'modules/footer.php'; ?>
