b67209073199d7ef3e1e517e907ecdb67411ba0f
Ryusei081 <2201125@s.asojuku.ac.jp>
2023-10-31 14:40:04 +0900

    プロフィール

--- source/profile.php
+++ source/profile.php
@@ -1,6 +1,7 @@
 <?php require 'modules/header.php'; ?>
  <h1 class="page_title">プロフィール編集</h1>
- <i class="fas fa-user-circle"></i>
+ <div class="centered_input_wide">
+ <i class="fas fa-user-circle fa-5x level-item"></i>
  <p><button class="" name="button">保存</button></p>
  <p>ユーザーネーム</p>
     <input placeholder="ユーザーネーム" type="text">
@@ -8,5 +9,6 @@
     <input placeholder="パスワード" type="password">
  <p>住所</p>
     <input placeholder="住所" type="address">
+</div>
 <?php require 'modules/navigation.php'; ?>
 <?php require 'modules/footer.php'; ?>
