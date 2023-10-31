524d990578aa6b52e5d50fecd74a320c0f8c492c
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2023-10-31 13:11:46 +0900

    デフォルトビューポートに変更

--- source/modules/header.php
+++ source/modules/header.php
@@ -2,7 +2,8 @@
 <html lang="ja">
 <head>
     <!-- スマホ用Viewport -->
-    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <!-- <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--　ビューポートテスト -->
     
     <title>ゲーミングデバイスEC</title>
 
