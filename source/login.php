7f63297cf8ee83a71f6a92dcc94487764c526b23
odahayato112 <hayato0112.0@icloud.com>
2023-10-31 14:19:17 +0900

    ログイン画面
    ログイン画面　修正　VueJsとPHPのみ

--- source/login.php
+++ source/login.php
@@ -1,15 +1,16 @@
 <?php require 'modules/header.php'; ?>
 
-    <h1>ログイン</h1>
-
+    <h1 class="page_title">ログイン</h1>
+<div class="centered_input_wide">
     <p>メールアドレス</p>
     <input placeholder="メールアドレス" type="text">
 
     <p>パスワード</p>
     <input placeholder="パスワード" type="password">
-
+</div>
     <br>
-    <button class="centered_button">新規登録</button>
+    <a href="signup.php" class="sinki_rinku">←新規登録</a><br>
+    <input class="centered_button" type="submit" value="マイページへ">
 
 <?php require 'modules/navigation.php'; ?>
 <?php require 'modules/footer.php'; ?>
