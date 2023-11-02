bafc1953c8e3c375714cda7702e58f4321d1b801
odahayato112 <hayato0112.0@icloud.com>
2023-11-02 12:47:53 +0900

    買い物

--- source/kaimonokago.php
+++ source/kaimonokago.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div class="kyc-search-bar">
-    <i class="material-icons md-21" id ="kyc-search-mark"></i>
-    <input class="kyc-search-box" type="text" placeholder="検索" autocomplete="off">
-</div>
+<?php require 'modules/header.php'; ?>
+<?php require 'modules/serach_box.php'; ?>
+<div class="centered_input_wide">
 <p>小計 ￥〇〇,〇〇〇</p>
-<p><input type="button" value="レジに進む(〇個の商品)[税込み]" id="button"></p>
-<div class="kaimono">
-    <img src="img/hito.jpg" height ="120" wight="120"/>
+<p><input type="button" value="レジに進む(〇個の商品)[税込み]" id="button"></p>    
+<img src="img/hito.jpg"/>
 </div>
+<?php require 'modules/footer.php'; ?>
