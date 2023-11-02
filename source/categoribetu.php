51b4203fe4a65b3eaf7960ea9e6c1e7d4808ed15
Ryusei081 <2201125@s.asojuku.ac.jp>
2023-11-02 11:57:15 +0900

    変更

--- source/categoribetu.php
+++ source/categoribetu.php
@@ -5,15 +5,15 @@
 <i class="fas fa-tv fa-5x "></i>
 </i>
 <br>
-<i class="level-item">
+
 <i class="fas fa-tv fa-5x "></i>
 <i class="fas fa-tv fa-5x "></i>
-</i>
+
 <br>
-<i class="level-item">
+
 <i class="fas fa-tv fa-5x "></i>
 <i class="fas fa-tv fa-5x "></i>
-</i>
+
     
 <?php require 'modules/navigation.php'; ?>
 <?php require 'modules/footer.php'; ?>
