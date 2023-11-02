040cfb7092739575c6082e48ab97f347228ff536
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2023-11-02 12:26:23 +0900

    category_list.js追加

--- source/category_list.php
+++ source/category_list.php
@@ -3,22 +3,18 @@
     <h3>使用用途</h3><br>
     <div class="category_div">
         仕事用　　　　　　個人用
-        <form>
+        <form id="app">
             <label><input type="radio" name="purpose" class="none"><i class="fas fa-chalkboard-teacher fa-5x"></i></label>
             <label><input type="radio" name="purpose" class="none"><i class="fas fa-gamepad fa-5x"></i></label>
             <p>デバイスブランド</p>
-            <label><input type="radio" name="bland" class="none"></label>
-            <label><input type="radio" name="bland" class="none"></label>
-            <label><input type="radio" name="bland" class="none"></label><br>
-            <label><input type="radio" name="bland" class="none"></label>
-            <label><input type="radio" name="bland" class="none"></label>
-            <label><input type="radio" name="bland" class="none"></label>
+            <label><input type="radio" name="bland" class="none"><img src=""></label>
+            <label><input type="radio" name="bland" class="none"><img src=""></label>
+            <label><input type="radio" name="bland" class="none"><img src=""></label><br>
+            <label><input type="radio" name="bland" class="none"><img src=""></label>
+            <label><input type="radio" name="bland" class="none"><img src=""></label>
+            <label><input type="radio" name="bland" class="none"><img src=""></label>
             <p>カテゴリから選ぶ</p>
-            <label><input type="radio" name="category" class="none"><i class="fas fa-mobile-alt fa-5x "></i></label>
-            <label><input type="radio" name="category" class="none"><i class="fas fa-mouse fa-5x "></i></label>
-            <label><input type="radio" name="category" class="none"><i class="fas fa-tv fa-5x"></i></label><br>
-            <label><input type="radio" name="category" class="none"><i class="fas fa-microphone-alt fa-5x"></i></label>
-            <label><input type="radio" name="category" class="none"><i class="fas fa-keyboard fa-5x"></i></label>
+            <label><input type="radio" name="category" class="none"><span v-for="item in category" :key="i"><i :class="item"></i></span></label>
             <label><input type="radio" name="category" class="none"></label>
             <p>価格</p>
             <label><input type="radio" name="price">0-1500円</label>　　　　<label><input type="radio" name="price">100000円以上</label><br>
@@ -27,6 +23,7 @@
             <label><input type="radio" name="price">50000-100000円</label>　　　　<button type="submit">検索</button><br>
         </form>
     </div>
+    <script src="./scripts/category_list.js"></script>
     <?php require 'modules/navigation.php'; ?>
     
 <?php require 'modules/footer.php'; ?>
