499f532f3c82624aff4ca797ce67f9e78a3df7aa
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2023-11-03 14:19:23 +0900

    カテゴリ一覧画面変更

--- source/category_list.php
+++ source/category_list.php
@@ -7,11 +7,11 @@
             <label><input type="radio" name="purpose" class="none"><i class="fas fa-chalkboard-teacher fa-5x"></i></label>
             <label><input type="radio" name="purpose" class="none"><i class="fas fa-gamepad fa-5x"></i></label>
             <p>デバイスブランド</p>
-            <label><input type="radio" name="bland" class="none"><span v-for="(image,i) in images" :key="i"><img :src="image"></span></label>
+            <label v-for="(image,i) in images" :bland_key="i"><input type="radio" name="bland" class="none"><img :src="image" class="category-image"></label>
             
             <p>カテゴリから選ぶ</p>
-            <label><input type="radio" name="category" class="none"><span v-for="(item,i) in category" :key="i"><i :class="item"></i></span></label>
-            <label><input type="radio" name="category" class="none"></label>
+            <span><a v-for="(item,j) in category" :category_key="j" href="categoribetu.php" class="categories"><i :class="item"></i></a></span>
+            <a href="categoribetu.php"></a>
             <p>価格</p>
             <label><input type="radio" name="price">0-1500円</label>　　　　<label><input type="radio" name="price">100000円以上</label><br>
             <label><input type="radio" name="price">1500-10000円</label><br>
