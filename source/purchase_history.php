b2a60595f362d5a87278c29b27ad7c378e038b50
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2023-11-06 11:27:38 +0900

    購入履歴追加,商品一覧画面名前変更

---
+++ source/purchase_history.php
@@ -0,0 +1,16 @@
+<?php require 'modules/header.php'; ?>
+<?php require 'modules/serach_box.php'?>
+
+<h1 class="page_title">購入履歴</h1>
+
+<div class="purchase_history">
+
+    <div class="item">
+        
+    </div>
+
+</div>
+
+
+<?php require 'modules/navigation.php';?>
+<?php require 'modules/footer.php'; ?>
