f73c314bf5e7fe0fcc061bc94c36312678783089
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2023-11-14 11:56:02 +0900

    DB名を全部小文字に変更

--- source/modules/db.php
+++ source/modules/db.php
@@ -1,7 +1,7 @@
 <!-- @英斗 誰のデータベース使うん？ -->
 <?php
  const SERVER = '127.0.0.1';
- const DBNAME = 'GamingDeviceEC';
+ const DBNAME = 'gamingdeviceec';
  const USER = 'root';
  const PASS = '';
  $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
