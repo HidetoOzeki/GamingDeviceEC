0569af60783ae87c0776a8d6f2694663398b1c02
takadasouma <takadasouma15@gmail.com>
2023-11-14 12:26:16 +0900

    a

--- source/mypage.php
+++ source/mypage.php
@@ -30,19 +30,20 @@
     </style>
 </head>
 <body>
-<form action="purchase_history.php">
-    <h1 class="page_title">マイページ</h1>
-    <p>ooo@oooooo</p>
- <div class="human">
-      <i class="fas fa-user-circle fa-10x"></i>
-</div>
-    <p>ユーザーネーム</p>
-<div class="link">
-    <a href="profile.php">プロフィール編集</a>
-</div>
-    <p><input type="submit" value="購入履歴" class="button"></p>
-      </form>
-    
+  <?php
+echo '<form action="purchase_history.php">';
+echo '<h1 class="page_title">マイページ</h1>';
+echo '<p>ooo@oooooo</p>';
+echo '<div class="human">';
+echo '<i class="fas fa-user-circle fa-10x"></i>';
+echo '</div>';
+echo '<p>ユーザーネーム</p>';
+echo '<div class="link">';
+echo '<a href="profile.php" >プロフィール編集</a>';
+echo '</div>';
+echo '<p><input type="submit" value="購入履歴" class="button"></p>';
+echo '</form>';
+    ?>
 <?php require 'modules/navigation.php'; ?> 
 </body>
 </html>
