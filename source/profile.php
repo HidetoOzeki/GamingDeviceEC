9729e18259513ca6722b20da5b3f9afcfd4a9b3a
Ryusei081 <2201125@s.asojuku.ac.jp>
2023-11-14 12:14:11 +0900

    echoにしました

--- source/profile.php
+++ source/profile.php
@@ -1,23 +1,25 @@
 <?php require 'modules/header.php'; ?>
- <h1 class="page_title">プロフィール編集</h1>
- <div class="centered_input_wide">
-   <p class="level-item">ooo@oooooo</p>
- <i class="fas fa-user-circle fa-8x level-item"></i>
- <style>
-   .b{
-      float: right;
-   }
- </style>
-<form action="mypage.php">
- <p><button class="b button is-link" name="button" >保存</button></p>
-</form>
- <br>
- <p>ユーザーネーム</p>
-    <input placeholder="ユーザーネーム" type="text">
- <p>パスワード</p>
-    <input placeholder="パスワード" type="password">
- <p>住所</p>
-    <input placeholder="住所" type="address">
-</div>
+<?php
+  echo '<h1 class="page_title">プロフィール編集</h1>';
+  echo '<div class="centered_input_wide">';
+      echo '<p class="level-item">ooo@oooooo</p>';
+   echo '<i class="fas fa-user-circle fa-8x level-item"></i>';
+   echo '<style>';
+    echo  '.b{';
+     echo    'float: right;';
+    echo  '}';
+   echo '</style>';
+   echo '<form action="mypage.php">';
+   echo '<p><button class="b button is-link" name="button" >保存</button></p>';
+   echo '</form>';
+   echo '<br>';
+   echo '<p>ユーザーネーム</p>';
+      echo '<input placeholder="ユーザーネーム" type="text">';
+   echo '<p>パスワード</p>';
+      echo '<input placeholder="パスワード" type="password">';
+   echo '<p>住所</p>';
+      echo '<input placeholder="住所" type="address">';
+   echo '</div>';
+?>
 <?php require 'modules/navigation.php'; ?>
 <?php require 'modules/footer.php'; ?>
