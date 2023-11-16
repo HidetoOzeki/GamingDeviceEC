615f1457a0ccec1460204e1fa13a52d54b4ea760
odahayato112 <hayato0112.0@icloud.com>
2023-11-16 12:43:57 +0900

    ほめてくれ

--- source/signup.php
+++ source/signup.php
@@ -1,8 +1,7 @@
 <?php require 'modules/header.php'; ?>
 <?php require 'modules/db.php'; ?>
-<?php 
-    $pdo = new PDO($connect,USER,PASS);
-    echo '<form action="mypage.php" method="post">';
+<?php
+    echo '<form action="signup.php" method="post">';
     echo '<h1 class="page_title">新規登録</h1>';
 
     echo '<div id="app">';
@@ -10,13 +9,14 @@
 
     echo '<div class="centered_input_wide">';
     echo '<p>ユーザーネーム</p>';
-    echo '<input placeholder="ユーザーネーム" type="text">';
+    echo '<input placeholder="ユーザーネーム" type="text" name="user_name">';
 
     echo '<p>メールアドレス</p>';
-    echo '<input placeholder="メールアドレス" type="text">';
+    echo '<input placeholder="メールアドレス" type="text" name="mail_address">';
 
     echo '<p>パスワード</p>';
-    echo '<input v-model="password" placeholder="パスワード" type="password"></div><br>';
+    echo '<input v-model="password" placeholder="パスワード" type="password" name="user_password">';
+    echo '</div><br>';
     echo '<button class="centered_button">登録</button><br>';
     echo '</form>';
     echo '<form action="shouhinichiran.php" method="post">';
@@ -26,5 +26,14 @@
     echo '</form>';
     echo '<script src="scripts/signup.js" type="text/javascript"></script>';
     ?>
+    <!--output-->
+<?php 
+    $pdo = new PDO($connect,USER,PASS);
+    $sql=$pdo->prepare('insert into user values(null,?,?,?,?,null,?)');
+    $sql->execute([
+        'U',$_POST['user_name'],$_POST['mail_address'],$_POST['user_password'],0]);
+
+    ?>
+<!--output-->
 
 <?php require 'modules/footer.php'; ?>
