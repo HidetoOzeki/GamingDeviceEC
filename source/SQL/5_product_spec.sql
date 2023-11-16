956f7e396a9fa63b1daba02578a881471c70d737
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2023-11-16 12:44:50 +0900

    compare機能作成途中

--- source/SQL/5_product_spec.sql
+++ source/SQL/5_product_spec.sql
@@ -2,7 +2,7 @@ CREATE TABLE product_spec(
 specification_id CHAR(5) NOT NULL,
 product_id INTEGER NOT NULL,
 interface VARCHAR(50) NOT NULL,
-screen_clearly VARCHAR(50) NOT NULL,
+clearly VARCHAR(50) NOT NULL,
 product_os VARCHAR(50) NOT NULL,
 product_size VARCHAR(30) NOT NULL,
 product_weight VARCHAR(30) NOT NULL,
