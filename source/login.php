fc62e8bb0650413f52c673e65f1df84c58f62543
fsho1222 <148302976+fsho1222@users.noreply.github.com>
2023-11-17 11:39:24 +0900

    a

--- source/login.php
+++ source/login.php
@@ -1,22 +1,32 @@
+<?php session_start(); ?>
 <?php require 'modules/header.php'; ?>
 
-    <h1 class="page_title">ログイン</h1>
-    <form action="mypage.php" method="post">
-    <div class="centered_input_wide">
-    <p>メールアドレス</p>
-    <input placeholder="メールアドレス" type="text">
+<?php
+$_SESSION['customer']=[
+        'id'=>$row['id'], 'name'=>$row['name'],
+        'address'=>$row['address'], 'login'=>$row['login'],
+        'password'=>$row['password']];
+}
+?>
+<?php
+    echo '<h1 class="page_title">ログイン</h1>';
+    echo '<form action="mypage.php" method="post">';
+    echo '<div class="centered_input_wide">';
+    echo '<p>メールアドレス</p>';
+    echo '<input placeholder="メールアドレス" type="text">';
 
-    <p>パスワード</p>
-    <input placeholder="パスワード" type="password">
-</div>
-    <br>
-    <a href="signup.php" class="sinki_rinku">←新規登録</a><br>
-    <input class="centered_button" type="submit" value="マイページへ">
-    </form>
+    echo '<p>パスワード</p>';
+    echo '<input placeholder="パスワード" type="password">';
+    echo '</div>';
+    echo '<br>';
+    echo '<a href="signup.php" class="sinki_rinku">←新規登録</a><br>';
+    echo '<input class="centered_button" type="submit" value="マイページへ">';
+    echo '</form>';
 
 
-    <p v-if="!isValidPassword" class="has-text-danger">入力内容が間違っています。メールアドレス、またはパスワードが違います。</p>
+    echo '<p v-if="!isValidPassword" class="has-text-danger">入力内容が間違っています。メールアドレス、またはパスワードが違います。</p>';
 
-    <script src="scripts/signup.js" type="text/javascript"></script>
+    echo '<script src="scripts/signup.js" type="text/javascript"></script>';
+?>
 
 <?php require 'modules/footer.php'; ?>
