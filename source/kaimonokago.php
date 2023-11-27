b98741c3bc146eb921fa96291064ad98131b7e76
Ryusei081 <2201125@s.asojuku.ac.jp>
2023-11-27 12:42:56 +0900

    ボタンを変更しました

--- source/kaimonokago.php
+++ source/kaimonokago.php
@@ -1,5 +1,6 @@
 <?php require 'modules/header.php'; ?>
 <?php require 'modules/serach_box.php'; ?>
+
 <div class="centered_input_wide">
     <p>小計 ￥〇〇,〇〇〇</p>
     <p><form action="kakutei.php" method="post"><input type="submit" value="レジに進む(〇個の商品)[税込み]" id="button"></form></p>    
@@ -8,7 +9,7 @@
             <h3>商品名</h3>
             <p>商品の値段</p>
             <p>その他詳細</p>
-            <form><button class="button_de" type="submit">削除</button></form>
+            <form><button class="button_de button is-danger is-outlined is-rounded" type="submit">削除</button></form>
             <div id="app" class="stepper-app">
                 <button class="decrease_btn" @click="decrease">-</button><div class="count"><input type="number" :value="count" min="1"></div><button @click="increase" class="increase_btn">+</button>
             </div>
