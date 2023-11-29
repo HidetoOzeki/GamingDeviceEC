93569f24f982158c500264d384b9fa92e5ff7ab8
odahayato112 <hayato0112.0@icloud.com>
2023-11-29 12:46:16 +0900

    a

--- source/and.php
+++ source/and.php
@@ -2,25 +2,23 @@
     <form action="toroku.php" method="post">
     <div class="centered_input_wide">
     
-    <p>メールアドレス</p>
+    <p>商品名</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>カテゴリーID</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>ブランドID</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>仕様用途</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>値段</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>フラグ（false）</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>スペックID</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>インターフェース</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
-    <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p></p>
     <input placeholder="" type="text" id="mail_address">
     <p>メールアドレス</p>
     <input placeholder="" type="text" id="mail_address">
@@ -36,6 +34,8 @@
     <input placeholder="" type="text" id="mail_address">
     <p>パスワード</p>
     <input placeholder="パスワード" name="password" type="password" id="user_password">
+    <p></p>
+    <input placeholder="" type="text" id="mail_address">
     </div>
    <br>
     <a href="signup.php" class="sinki_rinku">←新規登録</a><br>
