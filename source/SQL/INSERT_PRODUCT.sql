e36ea44b273588c9697a44d9377118f9dcc44c2e
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2023-11-30 12:02:44 +0900

    商品INSERT文のファイルアップ

---
+++ source/SQL/INSERT_PRODUCT.sql
@@ -0,0 +1,4 @@
+/* マウス */
+INSERT INTO product(category_id, bland_id, purpose_id, product_name, price, product_delete_flg) values("000001","000003","W","test_phone1",100000,"false");
+INSERT INTO product_spec values("000001",1,"インターフェース","解像度","OS","サイズ","重さ","色");
+INSERT INTO performance values("000005",5,11,6,3,NULL,50,8,NULL)
