d93c53d54838da0181381c5fb52f127113c3956a
odahayato112 <hayato0112.0@icloud.com>
2023-11-30 11:34:13 +0900

    dekima

--- source/and.php
+++ source/and.php
@@ -12,31 +12,37 @@
     <input placeholder="" type="text" id="mail_address">
     <p>値段</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>フラグ（false）</p>
-    <input placeholder="" type="text" id="mail_address">
     <p>スペックID</p>
     <input placeholder="" type="text" id="mail_address">
     <p>インターフェース</p>
     <input placeholder="" type="text" id="mail_address">
-    <p></p>
+    <p>スペック解像度</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>OS</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>スペックサイズ</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>スペック重さ</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>色</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>パフォーマンスID</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>メールアドレス</p>
+    <p>CPU</p>
     <input placeholder="" type="text" id="mail_address">
-    <p>パスワード</p>
+    <p>メモリ</p>
     <input placeholder="パスワード" name="password" type="password" id="user_password">
-    <p></p>
+    <p>ストレージ</p>
     <input placeholder="" type="text" id="mail_address">
-    </div>
+    <p>バッテリー</p>
+    <input placeholder="" type="text" id="mail_address">
+    <p>パフォーマンスサイズ</p>
+    <input placeholder="" type="text" id="mail_address">
+    <p>パフォーマンス解像度</p>
+    <input placeholder="" type="text" id="mail_address">
+    <p>パフォーマンス重さ</p>
+    <input placeholder="" type="text" id="mail_address">
+</div>
    <br>
     <a href="signup.php" class="sinki_rinku">←新規登録</a><br>
     <button type="button" class="centered_button button is-info is-outlined is-rounded" id="mypage_transition" >マイページへ<i class="far fa-hand-point-right"></i></button>
