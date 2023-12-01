df1f9819286800796fbf8795e53f6d28b5b08424
Ryusei081 <2201125@s.asojuku.ac.jp>
2023-12-01 12:28:15 +0900

    Update mousinsert.sql

--- source/SQL/mousinsert.sql
+++ source/SQL/mousinsert.sql
@@ -1,3 +1,20 @@
 
+
 INSERT INTO product(category_id, bland_id, purpose_id, product_name, price, product_delete_flg) 
-values("000002","000001","P","test_mouse",8000,"false");
+values("000002","000001","P","Razer Viper V3 HyperSpeed",11180,"false"),
+      ("000002","000001","P","Razer DeathAdder Essential",3880,"false"),
+      ("000002","000002","P","PRO X SUPERLIGHT 2",25410,"false"),
+      ("000002","000002","P","G402 HYPERION FURY",5610,"false"),
+      ("000002","000003","P","AEROX3",16278,"false"),
+      ("000002","000003","P","RIVAL 3",5918,"false"),
+      ("000002","000004","P","EC2-CW",15800,"false"),
+      ("000002","000004","P","FK2-C",8800,"false"),
+      ("000002","000005","P","KATAR PRO CH-930C011-AP",2882,"false"),
+      ("000002","000005","P","M65 RGB ULTRA WIRELESS",14000,"false"),
+      ("000002","000006","P","P709 ROG KERIS WIRELESS AIMPOINT",11980,"false"),
+      ("000002","000006","P","ROG Strix Impact II Moonlight White",6980,"false");
+
+INSERT INTO product_spec values("00001",1,"インターフェース","解像度","OS","サイズ","重さ","色"),
+      ("00001",1,"インターフェース","解像度","OS","サイズ","重さ","色"),
+
+INSERT INTO performance values("000001",1,NULL,NULL,NULL,NULL,150,50,5),
