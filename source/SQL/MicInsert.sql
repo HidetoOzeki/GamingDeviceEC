c9a308c9d913ded974a714c43946fb634a803c45
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2023-12-01 12:32:32 +0900

    BlueMIcrophoneのブランドID更新

--- source/SQL/MicInsert.sql
+++ source/SQL/MicInsert.sql
@@ -1,12 +1,12 @@
 
 INSERT INTO product(category_id, bland_id, purpose_id, product_name, price, product_delete_flg) 
-values("000004","000007","P","YETI X",28380,"false"),
+values("000004","000009","P","YETI X",28380,"false"),
 ("000004","000002","P","G733",21890,"false"),
 ("000004","000001","P","Razer BlackShark V2 X USB",9780,"false"),
 ("000004","000003","P","ALIAS PRO",51652,"false"),
 ("000004","000003","P","ARCTIS NOVA",29452,"false"),
 ("000004","000002","P","G735",27170,"false"),
-("000004","000007","P","YETI FOR THE AURORA COLLECTION",21780,"false"),
+("000004","000009","P","YETI FOR THE AURORA COLLECTION",21780,"false"),
 ("000004","000002","P","PRO X",13620,"false"),
 ("000004","000002","P","G331",5390,"false"),
 ("000004","000002","P","G333",28380,"false");
