af250af7ca2fdec89a322fc3dcc2915f2482f16e
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2023-12-07 10:44:28 +0900

    音声入力修正

--- source/modules/serach_box.php
+++ source/modules/serach_box.php
@@ -7,7 +7,11 @@ if(!('webkitSpeechRecognition' in window)){
 }else{
 var recognition = new webkitSpeechRecognition();
 
-var out = document.getElementById('output');
+var button = document.getElementById('voice_search');
+
+button.onclick = () => {
+    recognition.start();
+};
 
 //recognition.continuous = true;
 
@@ -32,7 +36,7 @@ recognition.onresult = (event) => {
     <form action="shouhinichiran.php" method="get" class="serch_form">
         <input type="text" name="product_name" class="search_box">
         <button type="submit" class="search_btn"><i class="fa fa-search fa-lg"></i></button>
-        <button type="button" id="voice_search" onclick="startRecog()"><i class="fa solid fa-microphone"></i></input>
+        <button type="button" id="voice_search"><i class="fa solid fa-microphone"></i></input>
     </form>
 </div>
 <div class="indent"></div>
